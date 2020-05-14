5873e44d404c507bc0c45b203c638bd34b852813
shoubizheng <zheng_shoubi@hotmail.com>
2020-05-14 21:55:06 +0800

    BurgerContiner layout - styling with css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Auxiliary from '../../hoc/Auxiliary'
+import classes from './Layout.css'
 const layout = (props) =>{
     return (
         <Auxiliary>
@@ -7,7 +8,7 @@ const layout = (props) =>{
                 Toolbar, SideDrawer, Backdrop
             </div>
 
-            <main>
+            <main className={classes.Content}>
                 {props.children}
             </main>
         </Auxiliary>
