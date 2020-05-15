b1d19940c2929a82ece1dde90614e2792ff5ff18
shoubizheng <zheng_shoubi@hotmail.com>
2020-05-15 21:43:41 +0800

    added burger build controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import BuildControl from './BuildControl/BuildControl'
+import classes from './BuildControls.css'
+
+const controlLabel = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+]
+
+const buildControls = (props)=>{
+    return(
+        <div className={classes.BuildControls}>
+            {controlLabel.map((lblItem) =>{return <BuildControl label={lblItem.label}/>})}
+        </div>
+    )
+}
+
+export default buildControls
