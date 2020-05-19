fbeb84f6d3989b54606832f4bb9a41599bd13647
Zheng <shoubi.zheng@dxc.com>
2020-05-19 17:45:38 +0800

    added order summary

---
+++ src/components/Burger/OderSummary/OrderSummary.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Auxiliary from '../../../components/../hoc/Auxiliary'
+const orderSummary = (props) =>{
+    console.log('[OrderSummary.js] props.ingredients' + props.ingredients.salad)
+    const ingredientList = Object.keys(props.ingredients).map(ingredientKey => 
+    {return <li key={ingredientKey}>
+        <span style={{textTransform: 'capitalize'}}>{ingredientKey}</span> : {props.ingredients[ingredientKey]}</li>})
+    console.log('[OrderSummary.js] ingredient li: ' + ingredientList)
+    return (
+
+        <Auxiliary>
+            <h3>Your Order Summary </h3>
+            <p>A burger with the following ingredients</p>
+            <ul>
+                {ingredientList}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </Auxiliary>
+    )   
+}
+
+export default orderSummary
