3dacced6023103cd394762ef1499d57749102d59
Zheng <shoubi.zheng@dxc.com>
2020-05-20 19:19:37 +0800

    added backdrop for side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Auxiliary from '../../hoc/Auxiliary'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
-const layout = (props) =>{
-    return (
-        <Auxiliary>
-            <Toolbar/>
-            <SideDrawer/>
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </Auxiliary>
-    )
+class Layout extends Component{
+    render(){
+        return (
+            <Auxiliary>
+                <Toolbar/>
+                <SideDrawer/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxiliary>
+        )
+    }
 }
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,16 +2,21 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import classes from './SideDrawer.css'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Auxiliary from '../../../hoc/Auxiliary'
 const sideDrawer=(props)=>{
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo/>
+        <Auxiliary>
+            <Backdrop show/>
+            <div className={classes.SideDrawer}>
+                <div className={classes.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
             </div>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+        </Auxiliary>
     )
 }
 
